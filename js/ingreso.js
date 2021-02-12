a7093eda61a80ca77b3bcf5c208284752a2a64fb
Nicolas Mauna <32561375+nima8998@users.noreply.github.com>
2021-02-12 14:09:16 -0300

    Se agrega jQuery en ingreso.js

--- js/ingreso.js
+++ js/ingreso.js
@@ -1,12 +1,12 @@
 registro = (event) => {
 	if (event.which == 13 || event.keyCode == 13) {
-		let nombreUsuario = document.getElementById("nombreUsuario");
-		sessionStorage.setItem("nombreUsuario", nombreUsuario.value);
-		let mailUsuario = document.getElementById("mailUsuario");
-		sessionStorage.setItem("mailUsuario", mailUsuario.value);
+		let nombreUsuario = $("#nombreUsuario");
+		sessionStorage.setItem("nombreUsuario", $(nombreUsuario).val());
+		let mailUsuario = $("#mailUsuario");
+		sessionStorage.setItem("mailUsuario", $(mailUsuario).val());
 		document.forms[0].submit();
 	} else if (event.which == 27 || event.keycode == 27) {
-		document.getElementById("modal").hidden = false;
+		$("#modal").hide();
 	}
 };
 
@@ -22,14 +22,14 @@ checkIn = () => {
 			"nombreUsuario"
 		)} !`;
 		document.getElementById("leftSection__usuario").appendChild(bienvenida);
-		document.getElementById("btnIngresar").style.display = "none";
-		document.getElementById("btnSalir").style.display = "block";
+		$("#btnIngresar").css("display", "none");
+		$("#btnSalir").css("display", "block");
 	}
 };
 
 checkOut = () => {
 	if (!sessionStorage.getItem("nombreUsuario")) {
-		document.getElementById("btnSalir").style.display = "none";
+		$("#btnSalir").css("display", "none");
 	}
 };
 
